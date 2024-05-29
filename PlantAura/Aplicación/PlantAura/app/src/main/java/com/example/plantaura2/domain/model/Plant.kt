27c6d89a8ffa65e680a1ee0e2451888b85504dbc
Villaaz <carlos.villaoslada.ceb@immune.institute>
2024-05-29 15:55:10 +0200

    forgot pass + mensaje pass incorrecta + pass ahora no se ve

--- PlantAura/Aplicación/PlantAura/app/src/main/java/com/example/plantaura2/domain/model/Plant.kt
+++
@@ -1,9 +0,0 @@
-package com.example.plantaura2.domain.model
-
-import com.google.firebase.firestore.IgnoreExtraProperties
-
-@IgnoreExtraProperties
-data class Plant(
-    val id: String = "",
-    val name: String = ""
-)
--- PlantAura/Aplicación/PlantAura/app/src/main/java/com/example/plantaura2/ui/questionHub/ui/QuestionHubViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.plantaura2.ui.questionHub.ui
-
-import androidx.lifecycle.ViewModel
-
-class QuestionHubViewModel : ViewModel() {
-
-}
